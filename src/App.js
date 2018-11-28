0247e2ee08f97a9c7fbc674f20c5d65869791535
Xtina <2006peacegypsy@gmail.com>
2018-11-27 19:23:23 -0800

    fixed, needs formatting

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
 import timelineData from "./data/timeline.json";
+
 import TimelineEvent from "./components/TimelineEvent";
 import Timeline from "./components/Timeline";
 
-const data = [
-  {
-    person: "Christina",
-    status: "Learning and succeeding",
-    timeline: "2 days ago"
-  }
-];
+// const data = [
+//   {
+//     person: "Christina",
+//     status: "Learning and succeeding",
+//     timeline: "2 days ago"
+//   }
+// ];
 
 class App extends Component {
   render() {
-    console.log(timelineData);
-    const statusComponents = data.map(status => {
-      return (
-        <TimelineEvent
-          name={status.person}
-          status={status.status}
-          timestamp={status.timestamp}
-          key={status.name}
-        />
-      );
-    });
+    console.log(timelineData.events);
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Ada Lovelaces social media</h1>
         </header>
         <main className="App-main" />
-        {statusComponents}
+        <Timeline data={timelineData} />
       </div>
     );
   }
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -5,18 +5,20 @@ import PropTypes from "prop-types";
 
 const TimelineEvent = props => {
   // Component functions always return JSX
+  const timeStamp = props.timeStamp;
+  const time = Timestamp(timeStamp);
   return (
     <section>
       <h3>{props.name}</h3>
       <h3>{props.status}</h3>
-      <h3>{props.timestamp}</h3>
+      <h3>{time}</h3>
     </section>
   );
 };
 TimelineEvent.propTypes = {
-  person: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-  timeline: PropTypes.string.isRequired
+  person: PropTypes.string,
+  status: PropTypes.string,
+  timeline: PropTypes.string
 };
 
 export default TimelineEvent;
